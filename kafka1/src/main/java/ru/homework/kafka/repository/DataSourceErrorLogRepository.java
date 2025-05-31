fd8b5a7599ede39e24a88ae161983280a586c0a4
xfn3t <n.semenuta4830@gmail.com>
2025-05-31 22:54:19 +0300

    Add Spring Data JPA repositories for User, TimeLimitExceedLog, and DataSourceErrorLog

--- kafka1/src/main/java/ru/homework/kafka/repository/DataSourceErrorLogRepository.java
+++ kafka1/src/main/java/ru/homework/kafka/repository/DataSourceErrorLogRepository.java
@@ -2,8 +2,8 @@ package ru.homework.kafka.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.homework.kafka.model.User;
+import ru.homework.kafka.model.DataSourceErrorLog;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface DataSourceErrorLogRepository extends JpaRepository<DataSourceErrorLog, Long> {
 }
--- kafka1/src/main/java/ru/homework/kafka/repository/TimeLimitExceedLogRepository.java
+++ kafka1/src/main/java/ru/homework/kafka/repository/TimeLimitExceedLogRepository.java
@@ -1,8 +1,8 @@
-package ru.t1.homework.cache.repository;
+package ru.homework.kafka.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.t1.homework.cache.model.TimeLimitExceedLog;
+import ru.homework.kafka.model.TimeLimitExceedLog;
 
 @Repository
 public interface TimeLimitExceedLogRepository extends JpaRepository<TimeLimitExceedLog, Long> {
