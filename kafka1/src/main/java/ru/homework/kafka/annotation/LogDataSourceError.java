e36c99aa47ef1e02b94deefee409bfd2ccc344b1
xfn3t <n.semenuta4830@gmail.com>
2025-05-31 22:55:54 +0300

    Introduce @LogDataSourceError annotation

---
+++ kafka1/src/main/java/ru/homework/kafka/annotation/LogDataSourceError.java
@@ -0,0 +1,8 @@
+package ru.homework.kafka.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface LogDataSourceError {}
