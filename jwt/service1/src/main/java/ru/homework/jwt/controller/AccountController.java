0506373e5aaae07ab77358b425f1ceb0f768b77a
xfn3t <n.semenuta4830@gmail.com>
2025-06-16 22:57:09 +0300

    Add REST controllers and exception handling

---
+++ jwt/service1/src/main/java/ru/homework/jwt/controller/AccountController.java
@@ -0,0 +1,40 @@
+package ru.homework.jwt.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.homework.jwt.dto.request.AccountRequestDto;
+import ru.homework.jwt.dto.response.AccountResponseDto;
+import ru.homework.jwt.service.AccountService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponseDto>> listAll() {
+        return ResponseEntity.ok(accountService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponseDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponseDto> create(@Valid @RequestBody AccountRequestDto dto) {
+        return ResponseEntity.status(201).body(accountService.create(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        accountService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ jwt/service1/src/main/java/ru/homework/jwt/controller/UserController.java
@@ -0,0 +1,55 @@
+package ru.homework.jwt.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ru.homework.jwt.dto.request.UserRequestDto;
+import ru.homework.jwt.dto.response.UserResponseDto;
+import ru.homework.jwt.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> listAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@Valid @RequestBody UserRequestDto dto) {
+        UserResponseDto created = userService.create(dto);
+        return ResponseEntity.status(201).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> update(
+            @PathVariable Long id,
+            @Valid @RequestBody UserRequestDto dto) {
+        return ResponseEntity.ok(userService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/heavy")
+    public ResponseEntity<Void> heavy() {
+        userService.someHeavyOperation();
+        return ResponseEntity.ok().build();
+    }
+}
