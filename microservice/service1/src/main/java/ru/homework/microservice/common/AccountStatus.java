7d1a771032d1cadaacbf8fb9fe47f92f43d9fdbd
xfn3t <n.semenuta4830@gmail.com>
2025-06-21 20:56:56 +0300

    service1: Add common directory

---
+++ microservice/service1/src/main/java/ru/homework/microservice/common/AccountStatus.java
@@ -0,0 +1,8 @@
+package ru.homework.microservice.common;
+
+public enum AccountStatus {
+    ARRESTED,
+    BLOCKED,
+    CLOSED,
+    OPEN
+}
---
+++ microservice/service1/src/main/java/ru/homework/microservice/common/TransactionStatus.java
@@ -0,0 +1,9 @@
+package ru.homework.microservice.common;
+
+public enum TransactionStatus {
+    ACCEPTED,
+    REJECTED,
+    BLOCKED,
+    CANCELLED,
+    REQUESTED
+}
